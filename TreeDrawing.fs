be27266dea52affeb453e21bd53a1bf5eb89388d
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-01-13 14:15:49 +0100

    Implemented the algorithm in the paper. Tested very briefly

--- TreeDrawing.fs
+++ TreeDrawing.fs
@@ -3,26 +3,26 @@ namespace TreeDrawing
 open TreeDrawing.Types
 
 module TreeDrawing =
-  let moveTree (Node ((l,xPos),childs)) x = (Node ((l,xPos+x),childs))
+  let moveTree ((Node ((l,xPos),childs)),x) = (Node ((l,xPos+x),childs))
 
-  let moveExtent (e : Extent) x =
-      printfn "%A" e
+  let moveExtent ((e : Extent),x) =
       List.map (fun (p,q) -> (p+x,q+x)) e
 
   // Merge two extents
-  let rec merge ps qs =
+  let rec merge (ps,qs) =
     match ps,qs with
       | [],qs   -> qs
       | ps,[]   -> ps
-      | ((p,_)::ps,(_,q)::qs) -> (p,q)::(merge ps qs)
+      | ((p,_)::ps,(_,q)::qs) -> (p,q)::(merge(ps,qs))
 
   // Merge n extents
-  let mergeList es = List.fold merge es
+  //let mergeList es = List.fold (fun acc e -> acc @ (merge e)) [] es
+  let mergeList es = List.fold (fun acc e -> merge(acc,e)) [] es
 
   // Fitting functions
   let rmax ((p : float),(q : float)) = if p > q then p else q
 
-  
+
   let rec fit e1 e2 =
     match e1,e2 with
       | ((_,p)::ps),((q,_)::qs) -> rmax (fit ps qs,(p - q + 1.0))
@@ -35,7 +35,7 @@ module TreeDrawing =
         | acc,e::es   ->
           let x = fit acc e
           in
-            x :: (fitlistl' (merge acc (moveExtent e x)) es)
+            x :: (fitlistl' (merge (acc,(moveExtent (e,x)))) es)
     in
       fitlistl' [] es
 
@@ -46,6 +46,55 @@ module TreeDrawing =
         | acc,e::es   ->
           let x = -(fit acc e)
           in
-            x :: (fitlistr' (merge acc (moveExtent e x)) es)
+            x :: (fitlistr' (merge (acc,(moveExtent(e,x)))) es)
     in
-      fitlistr' [] (List.rev es)
+      List.rev (fitlistr' [] (List.rev es))
+
+  let mean (x,y) = (x + y) / 2.0
+
+  let fitlist es = List.map mean (List.zip (fitlistr es) (fitlistl es))
+
+  // Design
+  let design tree =
+    let rec design' (Node(label,subtrees)) =
+      let (trees,extents) = List.unzip (List.map design' subtrees)
+      let positions = fitlist extents
+      let ptrees = List.map moveTree (List.zip trees positions)
+      let pextents = List.map moveExtent (List.zip extents positions)
+      let resultextent = (0.,0.) :: (mergeList pextents)
+      let resulttree = Node((label,0.),ptrees)
+      in
+        (resulttree,resultextent)
+    in
+      fst (design' tree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let f a = a
--- main.fsx
+++ main.fsx
@@ -13,8 +13,15 @@ let someTree = Node (("A",2.0),[])
 let someTree2 = Node ("A",[Node ("B",[]);Node ("C",[])])
 //printfn "%A" (TreeDrawing.TreeGeneration.generateRandomTree (4,10) 10)
 
-let someExtent = [(3.,3.);(1.,5.)]
+let someExtent = [(3.,3.);(1.,4.)]
 let someExtent2 = [(9.,9.);(6.,12.)]
 
-printfn "%A" (TreeDrawing.TreeDrawing.moveExtent someExtent 3.)
-printfn "%A" (TreeDrawing.TreeDrawing.merge someExtent someExtent2)
+printfn "%A" (TreeDrawing.TreeDrawing.moveExtent (someExtent,3.))
+printfn "%A" (TreeDrawing.TreeDrawing.merge (someExtent,someExtent2))
+printfn "%A" (TreeDrawing.TreeDrawing.mergeList [someExtent;someExtent2])
+printfn "%A" (TreeDrawing.TreeDrawing.fit someExtent someExtent2)
+
+
+printfn "%A" (TreeDrawing.TreeDrawing.design someTree2)
+
+//printfn "%A" (TreeDrawing.TreeDrawing.)
